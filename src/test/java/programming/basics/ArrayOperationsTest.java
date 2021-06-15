255031aca8d7733021dbab38c1725e3ef4d93c06
Pavan Segalla <segallapavan@gmail.com>
2021-06-15 18:37:41 +0530

    Adding files

--- src/test/java/programming/basics/ArrayOperationsTest.java
+++ src/test/java/programming/basics/ArrayOperationsTest.java
@@ -6,9 +6,9 @@ package programming.basics;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
+class ArrayOperationsTest {
     @Test void appHasAGreeting() {
-        App classUnderTest = new App();
+        ArrayOperations classUnderTest = new ArrayOperations();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
     }
 }
